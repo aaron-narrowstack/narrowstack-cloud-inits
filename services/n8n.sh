9f5d69f93583525f053737f5fac68c928c45c37a
Aaron Evans <aarone0621@gmail.com>
2026-04-12 17:00:30 -0500

    yaml to shell n8n

--- services/n8n.sh
+++ services/n8n.sh
@@ -1,15 +1,15 @@
 mkdir -p /opt/app/n8n
 touch /opt/app/n8n/docker-compose.yml
-cat > /opt/app/n8n/docker-compose.yml << 'N8N'
+cat > /opt/app/n8n/docker-compose.yml << N8N
 version: '3.8'
 services:
   n8n:
     image: n8nio/n8n
     container_name: n8n
     environment:
-      - N8N_HOST=n8n.aarondevelop.com
+      - N8N_HOST=${N8N_HOST}
       - N8N_PROTOCOL=https
-      - WEBHOOK_URL=https://n8n.aarondevelop.com/
+      - WEBHOOK_URL=${WEBHOOK_URL}
       - N8N_BASIC_AUTH_ACTIVE=true
       - N8N_BASIC_AUTH_USER=admin
       - N8N_BASIC_AUTH_PASSWORD=change-this-strong-password
@@ -30,3 +30,4 @@ networks:
 N8N
 mkdir -p /opt/app/n8n/n8n_data
 chown -R 1000:1000 /opt/app/n8n/n8n_data
+docker compose -f /opt/app/n8n/docker-compose.yml up -d
